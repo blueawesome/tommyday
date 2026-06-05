88e697e780b25c6bcc8b2b3ad49dbe8238139804
Tommy Day <day@meetmaestro.com>
2026-06-05 12:10:52 -0400

    Clean up generated page source

---
+++ src/scripts/legacyServiceWorkerCleanup.js
@@ -0,0 +1,15 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then((registrations) =>
+      Promise.all(registrations.map((registration) => registration.unregister()))
+    )
+    .catch(() => {});
+}
+
+if ('caches' in window) {
+  caches
+    .keys()
+    .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+    .catch(() => {});
+}
